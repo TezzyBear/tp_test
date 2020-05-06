c2048677df176971319cf957c29e10a2a5562156
Renzo Vargas <teztortsg@gmail.com>
2020-05-06 12:43:07 -0500

    Saca n puntos de la cara en un tiempo determinado

--- main.py
+++ main.py
@@ -14,12 +14,17 @@ cam_initialized = False
 cam_uptime = 2 #in seconds
 
 #Frame variables
-n_frames = 5
+n_frames = 3
+#frames_per_second = 20 
 frame_counter = 0
+
+
+
+prediction_points = []
 
 while True:
     time_start = time.time()
-    print(time_passed)
+    #print(time_passed)
     if time_passed > cam_uptime:
         break
     
@@ -39,10 +44,16 @@ while True:
         '''
         
         landmarks = predictor(gray, face)
+        face_points = []
         for i in range(0, 68):       
             cv2.circle(frame, (landmarks.part(i).x,landmarks.part(i).y), 4, (255, 0, 0), -1)
-
+            face_points.append((landmarks.part(i).x,landmarks.part(i).y))
+        prediction_points.append(face_points)
+        
     cv2.imshow("Frame", frame)
+
+
+
     if cam_initialized == False:
         cam_initialized = True
         time_start = time.time()
@@ -56,3 +67,16 @@ while True:
     frame_counter+=1
 
 print("Number of frames: " + str(frame_counter))
+
+frame_mod = frame_counter//n_frames
+frame_mod_res = frame_counter % n_frames
+print(frame_mod)
+
+temp = []
+
+for i in range (frame_counter - frame_mod_res):
+    if i % frame_mod == 0:
+        temp.append(prediction_points[i])
+prediction_points = temp
+print(len(prediction_points))
+
